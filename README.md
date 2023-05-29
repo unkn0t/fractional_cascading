ec523893cc38fff7136435c239575a89e91cbe99
Great1ng <warriorkesh4@gmail.com>
2023-05-29 20:15:50 +0600

    add docs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Implementation of Fractional Cascading DS
+
+### Benchmarks
+OS: Ubuntu 22.04.2 LTS x86_64<br>
+CPU: AMD Ryzen 5 4600H with Radeon Graphics (12) @ 3.000GHz<br>
+![Benchmark plot](./docs/lines.svg)
+
+### Based on:
+* [`wikipedia`](https://en.wikipedia.org/wiki/Fractional_cascading)
+* [`neerc wiki`](https://neerc.ifmo.ru/wiki/index.php?title=%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D0%BA%D0%B0_%D1%87%D0%B0%D1%81%D1%82%D0%B8%D1%87%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BA%D0%B0%D1%81%D0%BA%D0%B0%D0%B4%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,78 @@
 mod node;
-mod fractional_cascading;
 
-pub use fractional_cascading::FractionalCascading;
+use node::{merge_catalog_with_augmented, Node};
+
+#[derive(Debug)]
+pub struct FractionalCascading<'a, T> {
+    augmented_catalogs: Vec<Vec<Node<'a, T>>>,
+}
+
+impl<'a, T: Ord> FractionalCascading<'a, T> {
+    pub fn new(catalogs: &'a [Vec<T>]) -> Self {
+        debug_assert_ne!(catalogs.len(), 0, "Catalogs must have elements");
+
+        let catalogs_len = catalogs.len();
+        let mut augmented_catalogs = Vec::with_capacity(catalogs_len);
+
+        // Use unwrap instead unwrap_unchecked
+        // so function will be safe to call in release
+        augmented_catalogs.push(merge_catalog_with_augmented(
+            catalogs.last().unwrap(),
+            &[Node::fake(&catalogs[catalogs_len - 1])],
+        ));
+
+        for catalog in catalogs.iter().rev().skip(1) {
+            augmented_catalogs.push(merge_catalog_with_augmented(catalog.as_slice(), unsafe {
+                augmented_catalogs.last().unwrap_unchecked()
+            }));
+        }
+
+        augmented_catalogs.reverse();
+
+        Self { augmented_catalogs }
+    }
+
+    pub fn search(&self, key: &T) -> Vec<Option<usize>> {
+        let catalogs_len = self.augmented_catalogs.len();
+        let mut result = Vec::with_capacity(catalogs_len);
+
+        let mut index = self.augmented_catalogs[0].partition_point(
+            |node| unsafe { node.value(&self.augmented_catalogs[0][0]) } <= Some(key),
+        ) - 1;
+
+        let mut node = &self.augmented_catalogs[0][index];
+
+        result.push(node.closest_real_index(&self.augmented_catalogs[0]));
+
+        if node.is_real() {
+            index = node.prev;
+            node = &self.augmented_catalogs[0][index];
+        }
+
+        for i in 1..catalogs_len {
+            // go to next catalog
+            index = node.bridge;
+            node = &self.augmented_catalogs[i][index];
+
+            // try move to next node
+            if let Some(next_node) = self.augmented_catalogs[i].get(index + 1) {
+                if unsafe { next_node.value(&self.augmented_catalogs[i][0]) } <= Some(key) {
+                    node = next_node;
+                }
+            }
+
+            // pushing current node
+            result.push(node.closest_real_index(&self.augmented_catalogs[i]));
+
+            if node.is_real() {
+                index = node.prev;
+                node = &self.augmented_catalogs[i][index];
+            }
+        }
+
+        result
+    }
+}
 
 #[cfg(test)]
 mod unit_tests {
@@ -38,12 +109,13 @@ mod unit_tests {
 #[cfg(test)]
 mod stress_tests {
     use super::*;
-    use rand::rngs::StdRng;
-    use rand::{thread_rng, SeedableRng, Rng};
     use rand::distributions::uniform::SampleRange;
+    use rand::rngs::StdRng;
+    use rand::{thread_rng, Rng, SeedableRng};
 
-    fn random_catalogs<R>(count: usize, size_range: R, seed: u64) -> Vec<Vec<i32>> 
-    where R: SampleRange<usize> + Clone
+    fn random_catalogs<R>(count: usize, size_range: R, seed: u64) -> Vec<Vec<i32>>
+    where
+        R: SampleRange<usize> + Clone,
     {
         let mut rng = StdRng::seed_from_u64(seed);
 
@@ -65,10 +137,14 @@ mod stress_tests {
     fn find_with_binary_search<T: Ord + Copy>(catalogs: &[Vec<T>], key: &T) -> Vec<Option<T>> {
         catalogs
             .iter()
-            .map(|catalog| {
-                match catalog.binary_search(key) {
-                    Ok(index) => Some(catalog[index]),
-                    Err(index) => if index == 0 { None } else { Some(catalog[index - 1]) },
+            .map(|catalog| match catalog.binary_search(key) {
+                Ok(index) => Some(catalog[index]),
+                Err(index) => {
+                    if index == 0 {
+                        None
+                    } else {
+                        Some(catalog[index - 1])
+                    }
                 }
             })
             .collect()
