c1eead4fe6f73343b44f18472dd06cdf583efdcb
unkn0t <warriorkesh4@gmail.com>
2024-02-06 01:22:40 +0600

    better random_data test

--- tests/random_data.rs
+++ tests/random_data.rs
@@ -2,13 +2,14 @@ use std::ops::Range;
 
 use fractional_cascading::FCSearcher;
 
+use rand::distributions::{Uniform, Distribution};
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
 
 const SEED: Option<u64> = None;
-const N_ITERATIONS: usize = 10;
+const N_ITERATIONS: usize = 20;
 const N_SOURCES: usize = 100;
-const N_KEYS: usize = 10;
+const N_KEYS: usize = 20;
 const MAX_SRC_LEN: usize = 100;
 const VAL_RANGE: Range<i32> = -200..201;
 
@@ -17,11 +18,14 @@ fn main() {
     if let Some(seed) = SEED {
         let mut rng = SmallRng::seed_from_u64(seed);
         let mut sources = Vec::with_capacity(N_SOURCES);
+        let val_range = Uniform::from(VAL_RANGE);
 
         for _ in 0..N_SOURCES {
             let len = rng.gen_range(0..=MAX_SRC_LEN);
-            let mut src = vec![0; len];
-            rng.fill(src.as_mut_slice());
+            let mut src: Vec<_> = val_range
+                .sample_iter(&mut rng)
+                .take(len)
+                .collect();
             src.sort();
             sources.push(src);
         }
@@ -39,11 +43,14 @@ fn main() {
             let seed = rand::thread_rng().gen();
             let mut rng = SmallRng::seed_from_u64(seed);
             let mut sources = Vec::with_capacity(N_SOURCES);
+            let val_range = Uniform::from(VAL_RANGE);
 
             for _ in 0..N_SOURCES {
                 let len = rng.gen_range(0..=MAX_SRC_LEN);
-                let mut src = vec![0; len];
-                rng.fill(src.as_mut_slice());
+                let mut src: Vec<_> = val_range
+                    .sample_iter(&mut rng)
+                    .take(len)
+                    .collect();
                 src.sort();
                 sources.push(src);
             }
